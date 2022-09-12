a955906d232958458c48c7aa991decc5e7ee315d
Chipser <91829224+Chipser15@users.noreply.github.com>
2022-09-12 23:10:08 +0200

    Added: Attack event listener for GoblinEntity

--- src/main/java/net/earthspawn/mod/entities/classes/GoblinEntity.java
+++ src/main/java/net/earthspawn/mod/entities/classes/GoblinEntity.java
@@ -1,7 +1,8 @@
 package net.earthspawn.mod.entities.classes;
 
+import net.earthspawn.mod.Earthspawn;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -10,6 +11,10 @@ import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -18,6 +23,9 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
+import java.util.Objects;
+
+@Mod.EventBusSubscriber(modid = Earthspawn.MOD_ID)
 public class GoblinEntity extends Monster implements IAnimatable {
 
     private final AnimationFactory factory = new AnimationFactory(this);
@@ -30,7 +38,7 @@ public class GoblinEntity extends Monster implements IAnimatable {
         return Monster.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 12.0D)
                 .add(Attributes.ATTACK_DAMAGE, 2.5f)
-                .add(Attributes.ATTACK_SPEED, 2.0f)
+                .add(Attributes.ATTACK_SPEED, 1.0f)
                 .add(Attributes.MOVEMENT_SPEED, 0.20f)
                 .build();
     }
@@ -53,7 +61,11 @@ public class GoblinEntity extends Monster implements IAnimatable {
             return  PlayState.CONTINUE;
         }
         event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.goblin.idle", true));
-        return  PlayState.CONTINUE;
+        return PlayState.CONTINUE;
+    }
+
+    public boolean doHurtTarget(Entity target) {
+        return super.doHurtTarget(target);
     }
 
     @Override
